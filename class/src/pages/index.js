09ff22cab0ba6318aca35f811bec3bc22dfbd792
junheeLee <junhee2727@gmail.com>
2025-02-18 16:09:11 +0900

    feature main IconBar

--- class/src/pages/index.js
+++ class/src/pages/index.js
@@ -1,9 +1,13 @@
 import MainContainerLogic from "@/components/common/MainContainer/MainContainer.Container";
+import { Main__IconBar } from "./styles";
 
 export default function Home() {
   return (
     <MainContainerLogic>
-      
+      <Main__IconBar>
+        <img src = "/image/Vector.png"/>
+        <img src = "/image/material-symbols_settings.png"/>
+      </Main__IconBar>
     </MainContainerLogic>
   );
 }
---
+++ class/src/pages/styles.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled"
+
+export const Main__IconBar = styled.div`
+    width:100%;
+    padding-right:16px;
+
+    display:flex;
+    flex-direction:row-reverse;
+    gap:16px;
+`
