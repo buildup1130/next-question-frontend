e5a47e0ca21141479e2b876baccd593dd1675214
junheeLee <junhee2727@gmail.com>
2025-05-12 20:18:39 +0900

    오류 수정정

--- class/src/components/common/BookShelf/BookShelf.Styles.js
+++ class/src/components/common/BookShelf/BookShelf.Styles.js
@@ -15,11 +15,11 @@ export const Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 12px 0 8px 0;
+  padding: 0px 0 8px 0;
 `;
 
 export const Title = styled.h1`
-  font-size: 18px;
+  font-size: 22px;
   font-weight: 500;
   margin: 0;
   padding: 0;
