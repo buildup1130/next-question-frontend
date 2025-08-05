fdec4345b194072310906ba956996b6d8dc7fc3c
Alonier_mac <junhee2727@gmail.com>
2025-08-05 10:13:41 +0900

    meta view port app.js 로 이동

--- class/src/pages/_app.js
+++ class/src/pages/_app.js
@@ -6,6 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
 import BottomNavigationLogic from "@/components/common/BottomNavigation/BottomNavigation.Container";
+import Head from "next/head";
 
 const hideBottomNavRoutes = ['/Question']
 
@@ -14,6 +15,10 @@ export default function App({ Component, pageProps }) {
 
   const shouldShowBottomNav = !hideBottomNavRoutes.includes(router.pathname);
   return (
+    <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0"/>
+    </Head>
     <AuthProvider>
       <AnimatePresence mode="wait" initial={false}>
         <motion.div
@@ -35,5 +40,6 @@ export default function App({ Component, pageProps }) {
       {shouldShowBottomNav && <BottomNavigationLogic></BottomNavigationLogic>}
       <ToastContainer position="bottom-center" autoClose={2000} />
     </AuthProvider>
+    </>
   );
 }
